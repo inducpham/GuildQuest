98a15dec64a7865044d80a265b60b17f99f3c886
unknown <sai.internship@SAIWKS1474.sai.gameloft.org>
2012-08-07 11:39:17 +0700

    Improve the base render methods
    The render functions now hide the glyphWidth and glyphHeight variables

--- src/render/facade.lua
+++ src/render/facade.lua
@@ -3,44 +3,58 @@
 assert(love, "love required");
 assert(love.graphics, "the love.graphics module was required");
 
-render = {};
+----------------------------------------------------------------------
+-- Định nghĩa các biến local để giấu thông tin
+----------------------------------------------------------------------
 
-render.glyphWidth, render.glyphHeight = 10, 10;
+local glyphWidth, glyphHeight = 10, 10;
 
 ----------------------------------------------------------------------
 -- Hàm này load font vào hệ thống cũng như ghi lại các thông số
 -- cần thiết để render sau này
 ----------------------------------------------------------------------
-function render.initWithFont(facename, facesize)
+
+local function initWithFont(facename, facesize)
 	-- Create the new font
 	local f = love.graphics.newFont(facename, facesize);
 	love.graphics.setFont(f);
 	love.graphics.setColor(200,200,200,255);
     -- Get the font glyph data
-	render.glyphWidth = f:getWidth(" ");
-	render.glyphHeight = f:getHeight();
+	glyphWidth = f:getWidth(" ");
+	glyphHeight = f:getHeight();
 end
 
 ----------------------------------------------------------------------
 -- Hàm này dùng để lấy kích cỡ của 1 ô ký tự
 ----------------------------------------------------------------------
-function render.getGlyphSize()
-    return render.glyphWidth, render.glyphHeight;
+
+local function getGlyphSize()
+    return glyphWidth, glyphHeight;
 end
 
 ----------------------------------------------------------------------
 -- Hàm này dùng để tính toán kích cỡ màn hình dựa theo số ký tự
 -- ngang và số ký tự dọc
 ----------------------------------------------------------------------
-function render.calculateSize(width, height)
-    return render.glyphWidth * width, render.glyphHeight * height
+
+local function calculateSize(width, height)
+    return glyphWidth * width, glyphHeight * height;
 end
 
 ----------------------------------------------------------------------
 -- Hàm này dùng để ghi 1 đoạn ký tự theo vị trí ô
 ----------------------------------------------------------------------
-function render.drawStringAt( str, x, y)
-    love.graphics.print(str,
-        render.glyphWidth * x,
-        render.glyphHeight * y);
+
+local function drawStringAt( str, x, y)
+    love.graphics.print(str, glyphWidth * x, glyphHeight * y);
 end
+
+----------------------------------------------------------------------
+-- Bọc các hàm trong gói hàm render, dồng thời giấu thông tin
+----------------------------------------------------------------------
+
+render = {};
+render.initWithFont = initWithFont;
+render.getGlyphSize = getGlyphSize;
+render.calculateSize = calculateSize;
+render.drawStringAt = drawStringAt;
